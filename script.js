cc3c596bcc1d3490eae10b600a8370a35c7d40d4
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2024-12-02 09:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function mincost(arr)
 	let heap=[...arr]; // copy the array to the heap
 	heap.sort((a, b) => a - b); // to make the heap in the ascending order
 	while(heap.length > 1){
-		const first=heap.shift;
-		const second=heap.shift;
+		const first=heap.shift();
+		const second=heap.shift();
 		totalcost+=first+second;
 		heap.push(first+second);
 	heap.sort((a, b) => a - b); // to make the heap in the ascending order
