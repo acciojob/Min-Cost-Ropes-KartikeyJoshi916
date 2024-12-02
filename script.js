feb223b6165031a2b27d1c053ec08271633250ef
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2024-12-02 08:58:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  let arrSorted=arr.sort();
+	let sum=0;
+	arrSorted.map((elem,i)=>{
+		sum+=elem;
+	});
+	return sum;
 }
 
 module.exports=mincost;
